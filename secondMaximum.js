688ca237383571c4c0e21974c8271c8d04d1912d
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2024-04-02 00:09:27 -0400

    first day

---
+++ secondMaximum.js
@@ -0,0 +1,32 @@
+// Given an array Arr of size N, print the second largest distinct element from an array. If the second largest element doesn't exist then return -1.
+
+// Example 1:
+
+// Input:
+// N = 6
+// Arr[] = {12, 35, 1, 10, 34, 1}
+// Output: 34
+// Explanation: The largest element of the
+// array is 35 and the second largest element
+// is 34.
+
+function secondMaximum(N, arr) {
+  var secondMax = -1;
+  var max = arr[0];
+  for (var i = 0; i < n; i++) {
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] > secondMax && arr[i] < max) {
+      secondMax = arr[i];
+    }
+  }
+  return secondMax;
+}
+
+console.log(secondMaximum(N, arr));
+let N = 6;
+let arr = [12, 35, 1, 10, 34, 1];
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(1)
